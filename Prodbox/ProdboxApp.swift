5861dc6520b3d93c6c62aa324f29b0889c722382
N1femi <niffybabs@gmail.com>
2026-08-16 10:13:30 -0400

    Initial Commit

---
+++ Prodbox/ProdboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProdboxApp.swift
+//  Prodbox
+//
+//  Created by Nifemi Babajide on 8/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ProdboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
