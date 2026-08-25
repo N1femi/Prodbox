c8166e059ecf6fa0e910eca5dafec80120df0c75
N1femi <niffybabs@gmail.com>
2026-08-25 07:05:58 -0400

    Starting testing reminders system

---
+++ Prodbox/Components/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminder.swift
+//  Prodbox
+//
+//  Created by Nifemi Babajide on 8/25/26.
+//
+
+import Foundation
+
+struct Reminder: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let description: String
+    let parent: UUID?
+}
---
+++ Prodbox/TabViews/ReminderView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  Prodbox
+//
+//  Created by Nifemi Babajide on 8/25/26.
+//
+
+import SwiftUI
+
+struct ReminderView: View {
+    let testReminders: [Reminder] = [
+        Reminder(title: "Take out Trash", description: "Cleaning", parent: nil),
+        
+        Reminder(title: "Get groceries", description: "Food time", parent: nil),
+        
+        Reminder(title: "Make bed", description: "Start the day", parent: nil)
+    ]
+    
+    var body: some View {
+        List(testReminders) { reminder in
+            Text(reminder.title)
+        }
+    }
+}
+
+#Preview {
+    ReminderView()
+}
